be8acbe82d651a5672fb7531940d09e869b4dd47
vishnuprasandh <141708981+vishnuprasandh@users.noreply.github.com>
2023-10-03 18:31:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import "./App.css";
+
+// Material UI imports
+import Chip from "@mui/material/Chip";
+import FaceIcon from "@mui/icons-material/Face";
+import Paper from "@mui/material/Paper";
+import LockIcon from "@mui/icons-material/Lock";
+
+import Switch from "@mui/material/Switch";
+import { useState } from "react";
+import Login from "./formControl/login";
+import Signup from "./formControl/signup";
+
+function App() {
+  const [checked, setChecked] = useState(true);
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  return (
+    <div className="App">
+      <Paper elevation={3} style={{ padding: "10px", paddingBottom: "50px" }}>
+        <div align="center">
+          {checked ? (
+            <Chip
+              icon={<LockIcon />}
+              label="Log In"
+              variant="outlined"
+              color="info"
+            />
+          ) : (
+            <Chip
+              icon={<FaceIcon />}
+              label="Sign Up"
+              variant="outlined"
+              color="info"
+            />
+          )}
+          <br />
+
+          <Switch
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ "aria-label": "controlled" }}
+          />
+        </div>
+
+        {checked ? <Login /> : <Signup />}
+      </Paper>
+    </div>
+  );
+}
+
+export default App;
